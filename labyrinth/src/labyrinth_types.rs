46a39a49a438c117c7025481b74516906e0a403b
Felix Wittwer <dev@felixwittwer.de>
2019-03-04 11:06:22 +0100

    Implement input file parsing

--- labyrinth/src/labyrinth_types.rs
+++ labyrinth/src/labyrinth_types.rs
@@ -3,10 +3,11 @@
 pub struct Point {
     pub x: usize,
     pub y: usize,
-    pub z: usize
+    pub z: usize,
 }
 
 /// The central maze data structure
+#[derive(Debug)]
 pub struct Maze {
     /// The Grid we are working on
     pub grid: Grid,
@@ -15,24 +16,40 @@ pub struct Maze {
     /// Paths already mapped into the grid
     pub paths: Vec<Path>,
     /// Paths that have not been mapped yet
-    pub unmapped_paths: Vec<(Point, Point)>
+    pub unmapped_paths: Vec<(Point, Point)>,
+}
+
+impl Maze {
+    pub fn new(
+        dimensions: Point,
+        paths: Vec<(Point, Point)>,
+        obstacles: Option<Vec<Point>>,
+    ) -> Self {
+        Maze {
+            grid: initialize_grid(dimensions.x, dimensions.y, dimensions.z, &obstacles),
+            obstacles: obstacles,
+            paths: Vec::with_capacity(paths.len()),
+            unmapped_paths: paths,
+        }
+    }
 }
 
 /// A single path in the maze.
+#[derive(Debug)]
 pub struct Path {
     /// Starting point
     start: Point,
     /// Ending point
     end: Point,
     /// All points to be visitad between start and end
-    path: Vec<Point>
+    path: Vec<Point>,
 }
 
 /// The grid the maze is set up in. Contains for every field the information about its state.
 pub type Grid = Vec<Vec<Vec<Field>>>;
 
-#[derive(Debug, Clone)]
 /// A single field. Can be either free or used or it may be a wall.
+#[derive(Debug, Clone)]
 pub enum Field {
     Free,
     Used,
@@ -42,7 +59,12 @@ pub enum Field {
 /// Initializes a new, empty grid, optionally with pre-defined walls which can be provided using the `walls` parameter.
 ///
 /// The width defines the domain of the x-axis, the depth the domain of the y axis and depth the z-axis.
-pub fn initialize_grid(width: usize, height: usize, depth: usize, walls: Option<&Vec<Point>>) -> Grid {
+pub fn initialize_grid(
+    width: usize,
+    height: usize,
+    depth: usize,
+    walls: &Option<Vec<Point>>,
+) -> Grid {
     let mut grid = vec![vec![vec![Field::Free; depth]; height]; width];
 
     // place walls if any
--- labyrinth/src/main.rs
+++ labyrinth/src/main.rs
@@ -1,5 +1,32 @@
 mod labyrinth_types;
+mod parser;
+
+use clap::{App, Arg};
+use labyrinth_types::Maze;
 
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Labyrinth Benchmark")
+        .version("1.0")
+        .author("Felix Wittwer <dev@felixwittwer.de>")
+        .about("A Rust port of the labyrinth benchmark from the STAMP collection.")
+        .arg(
+            Arg::with_name("threads")
+                .short("t")
+                .value_name("usize")
+                .help("Defines the number of threads to use.")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Input file to describe the grid and paths.")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+
+    let input_file = matches.value_of("INPUT").unwrap();
+
+    let (dimensions, paths) = parser::parse_file(input_file);
+
+    let mut maze = Maze::new(dimensions, paths, None);
 }
