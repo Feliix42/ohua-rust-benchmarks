0c86ddc0fb0ed1d40dc362b56a6d6436cef2515a
Felix Wittwer <dev@felixwittwer.de>
2019-10-14 10:59:06 +0200

    workaround to reduce load

--- intruder/src/bin/stm.rs
+++ intruder/src/bin/stm.rs
@@ -7,7 +7,7 @@ use std::fs::{create_dir_all, File};
 use std::io::Write;
 use std::str::FromStr;
 use std::thread;
-use stm::{atomically, TVar};
+use stm::{atomically}; //, TVar};
 use time::PreciseTime;
 
 fn main() {
@@ -110,7 +110,8 @@ fn main() {
 
     for r in 0..runs {
         // prepare the data for the run
-        let input_data = TVar::new(input.clone());
+        // let input_data = TVar::new(input.clone());
+        let input_data = input.clone();
 
         // start the clock
         let start = PreciseTime::now();
@@ -181,47 +182,74 @@ fn main() {
 /// Everything inside this function is being timed.
 ///
 /// Returns a Vec of flow IDs that contained an attack for later check
-fn analyze_stream(packets: TVar<VecDeque<Packet>>, decoder_state: StmDecoderState) -> Vec<usize> {
+fn analyze_stream(mut packets: VecDeque<Packet>, decoder_state: StmDecoderState) -> Vec<usize> {
     let mut found_attacks = Vec::new();
 
-    loop {
-        let packet = atomically(|trans| {
-            let mut v = packets.read(trans)?;
-            if v.len() == 0 {
-                Ok(None)
-            } else {
-                let r = v.pop_front().unwrap();
-                packets.write(trans, v)?;
-                Ok(Some(r))
-            }
-        });
+    // NOTE: This is a deviation from the original code where packets where
+    // retrieved individually. Unfortunately, this made up for 96% of all
+    // processing time, hence the simplification
+    // loop {
+    //     let packet = atomically(|trans| {
+    //         let mut v = packets.read(trans)?;
+    //         if v.len() == 0 {
+    //             Ok(None)
+    //         } else {
+    //             let r = v.pop_front().unwrap();
+    //             packets.write(trans, v)?;
+    //             Ok(Some(r))
+    //         }
+    //     });
 
-        if let Some(p) = packet {
-            // do the algorithm
-            let decoder_result = atomically(|trans2| decode_packet(&p, &decoder_state, trans2));
-            if let Some(decoded_flow) = decoder_result {
-                // process the output -> run the detector
-                if run_detector(&decoded_flow.data) == DetectorResult::SignatureMatch {
-                    found_attacks.push(decoded_flow.flow_id);
-                }
+    for p in packets.drain(..) {
+        // if let Some(p) = packet {
+        // do the algorithm
+        let decoder_result = atomically(|trans2| decode_packet(&p, &decoder_state, trans2));
+        if let Some(decoded_flow) = decoder_result {
+            // process the output -> run the detector
+            if run_detector(&decoded_flow.data) == DetectorResult::SignatureMatch {
+                found_attacks.push(decoded_flow.flow_id);
             }
-        } else {
-            break;
         }
+        // } else {
+        //     break;
+        // }
     }
 
     found_attacks
 }
 
-fn run_eval(packets: TVar<VecDeque<Packet>>, threadcount: usize) -> Vec<usize> {
+fn partition_input_vec(mut packets: VecDeque<Packet>, threadcount: usize) -> Vec<VecDeque<Packet>> {
+    let l = packets.len() / threadcount;
+    let mut rest = packets.len() % threadcount;
+
+    let mut partitioned = vec![VecDeque::with_capacity(l); threadcount];
+
+    for t_num in 0..threadcount {
+        if rest > 0 {
+            partitioned[t_num] = packets.split_off(packets.len() - l - 1);
+            rest -= 1;
+        } else {
+            if packets.len() <= l {
+                partitioned[t_num] = packets.split_off(0);
+            } else {
+                partitioned[t_num] = packets.split_off(packets.len() - l);
+            }
+        }
+    }
+
+    partitioned
+}
+
+fn run_eval(packets: VecDeque<Packet>, threadcount: usize) -> Vec<usize> {
     let mut found_attacks = Vec::new();
     let decoder_state = StmDecoderState::new();
 
     let mut handles = Vec::with_capacity(threadcount);
-    for _ in 0..threadcount {
-        let ps = packets.clone();
+    let mut inputs = partition_input_vec(packets, threadcount);
+
+    for packets in inputs.drain(..) {
         let ds = decoder_state.clone();
-        handles.push(thread::spawn(move || analyze_stream(ps, ds)));
+        handles.push(thread::spawn(move || analyze_stream(packets, ds)));
     }
 
     for handle in handles {
