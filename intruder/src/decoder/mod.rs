922e6f6ec607270e31256ac2ea6404b23e2f2f2b
Felix Wittwer <dev@felixwittwer.de>
2019-11-22 14:58:12 +0100

    Add split generator and bench scripts

--- intruder/src/decoder/mod.rs
+++ intruder/src/decoder/mod.rs
@@ -4,6 +4,7 @@ pub mod simple;
 #[cfg(feature = "transactional")]
 pub mod stm_decoder;
 
+#[cfg_attr(feature = "ohua", derive(Clone))]
 #[derive(PartialEq, Debug)]
 pub struct DecodedPacket {
     pub flow_id: usize,
