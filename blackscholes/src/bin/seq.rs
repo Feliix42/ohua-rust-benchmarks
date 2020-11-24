df342c54c83ef3769a2a674bc38cca36b39c4fad
Felix Wittwer <dev@felixwittwer.de>
2020-11-24 16:50:39 +0100

    Implement sequential blackscholes

---
+++ blackscholes/src/bin/seq.rs
@@ -0,0 +1,126 @@
+use blackscholes::{self, OptionData};
+use clap::{App, Arg};
+use cpu_time::ProcessTime;
+use std::str::FromStr;
+use std::time::Instant;
+
+fn main() {
+    let matches = App::new("Sequential blackscholes benchmark")
+        .version("1.0")
+        .author("Felix Wittwer <dev@felixwittwer.de>")
+        .about("A Rust port of the blackscholes benchmark from the PARSEC collection, implemented in a sequential manner.")
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Input file describing the stock options to trade.")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("runs")
+                .long("runs")
+                .short("r")
+                .takes_value(true)
+                .help("The number of runs to conduct.")
+                .default_value("1")
+        )
+        .arg(
+            Arg::with_name("json")
+                .long("json")
+                .short("j")
+                .help("Dump results as JSON file.")
+        )
+        .arg(
+            Arg::with_name("verify")
+            .long("verify")
+            .short("v").help("Verify the calculated prices")
+        )
+        .arg(
+            Arg::with_name("outdir")
+                .long("outdir")
+                .short("o")
+                .help("Sets the output directory for JSON dumps")
+                .takes_value(true)
+                .default_value("results")
+        )
+        .get_matches();
+
+    // parse parameters
+    let input_file = matches.value_of("INPUT").unwrap();
+
+    // parse runtime parameters
+    let runs =
+        usize::from_str(matches.value_of("runs").unwrap()).expect("Could not parse number of runs");
+    let verify = matches.is_present("verify");
+    let json_dump = matches.is_present("json");
+    let out_dir = matches.value_of("outdir").unwrap();
+
+    // read and parse input data
+    let input_data = OptionData::load_from_file(input_file).unwrap();
+
+    if !json_dump {
+        println!("[info] Loaded {} options.", input_data.len());
+    }
+
+    // run the benchmark itself
+    let mut results = Vec::with_capacity(runs);
+    let mut cpu_time = Vec::with_capacity(runs);
+
+    if !json_dump {
+        print!("[info] Running benchmark");
+    }
+
+    for _ in 0..runs {
+        // clone the necessary data
+        let options = input_data.clone();
+
+        // start the clock
+        let cpu_start = ProcessTime::now();
+        let start = Instant::now();
+
+        // run the algorithm
+        let res = run_blackcholes(options);
+
+        // stop the clock
+        let cpu_end = ProcessTime::now();
+        let end = Instant::now();
+        let runtime_ms = end.duration_since(start).as_millis();
+        let cpu_runtime_ms = cpu_end.duration_since(cpu_start).as_millis();
+
+        if !json_dump {
+            print!(".");
+        }
+
+        results.push(runtime_ms);
+        cpu_time.push(cpu_runtime_ms);
+
+        // optionally run the verification
+        if verify {
+            let err_count = blackscholes::verify_all_results(&input_data, &res);
+            if err_count != 0 {
+                eprintln!("[error] Encountered {} errors in calculation.", err_count);
+            }
+        }
+    }
+
+    // write output
+    if json_dump {
+        //TODO
+    } else {
+        println!(" done!");
+
+        println!("[info] All runs completed.");
+        println!("\nStatistics:");
+        println!("    Number of options: {}", input_data.len());
+        println!("    Input file used: {}", input_file);
+        println!("    Runs: {}", runs);
+        println!("\nCPU-time used (ms): {:?}", cpu_time);
+        println!("Runtime (ms): {:?}", results);
+    }
+}
+
+fn run_blackcholes(options: Vec<OptionData>) -> Vec<f32> {
+    options
+        .iter()
+        .map(OptionData::calculate_black_scholes)
+        .collect()
+}
--- blackscholes/src/lib.rs
+++ blackscholes/src/lib.rs
@@ -1,12 +1,15 @@
-use std::io::BufReader;
-use std::io::prelude::*;
 use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 use std::str::FromStr;
 
+const INV_SQRT_2PI: f32 = 0.39894228040143270286;
+
 /// Type of a stock option
+#[derive(PartialEq, Clone)]
 pub enum OptionType {
     Put,
-    Call
+    Call,
 }
 
 impl From<&str> for OptionType {
@@ -14,11 +17,12 @@ impl From<&str> for OptionType {
         match inp {
             "P" => Self::Put,
             "C" => Self::Call,
-            _ => panic!("Incorrect Option type (expected P or C)")
+            _ => panic!("Incorrect Option type (expected P or C)"),
         }
     }
 }
 
+#[derive(Clone)]
 pub struct OptionData {
     /// Spot Price
     pub spot: f32,
@@ -67,7 +71,7 @@ impl OptionData {
 
         let mut buf = String::new();
         reader.read_line(&mut buf)?;
-        let line_count = usize::from_str(&buf).unwrap();
+        let line_count = usize::from_str(buf.trim()).unwrap();
 
         let mut options = Vec::with_capacity(line_count);
 
@@ -78,4 +82,91 @@ impl OptionData {
 
         Ok(options)
     }
+
+    pub fn calculate_black_scholes(&self) -> f32 {
+        // just the 1:1 copyover of the calculation in the C version
+        let x_sqrt_time = self.time.sqrt();
+        let x_log_term = (self.spot / self.strike).ln();
+
+        let x_power_term = self.volatility.powi(2) * 0.5;
+
+        let mut x_d1 = (self.interest + x_power_term) * self.time + x_log_term;
+
+        let x_den = self.volatility * x_sqrt_time;
+        x_d1 /= x_den;
+        let x_d2 = x_d1 - x_den;
+
+        let no_fx_d1 = cndf(x_d1);
+        let no_fx_d2 = cndf(x_d2);
+
+        let future_value_x = self.strike * (-self.interest * self.time).exp();
+
+        let option_price = if self.ty == OptionType::Call {
+            (self.spot * no_fx_d1) - (future_value_x * no_fx_d2)
+        } else {
+            // PUT option
+            let neg_no_fx_d1 = 1.0 - no_fx_d1;
+            let neg_no_fx_d2 = 1.0 - no_fx_d2;
+
+            (future_value_x * neg_no_fx_d2) - (self.spot * neg_no_fx_d1)
+        };
+
+        option_price
+    }
+
+    /// Returns `true` when the divergence between the computed price and the DerivaGem Reference Value is within the acceptable tolerance of 0.0001.
+    #[inline(always)]
+    fn verify(&self, result: f32) -> bool {
+        let delta = (self.ref_val - result).abs();
+        delta < 0.0001
+    }
+}
+
+/// Cumulative Normal Distribution Function
+///
+/// https://en.wikipedia.org/wiki/Cumulative_distribution_function
+fn cndf(input_x: f32) -> f32 {
+    let invert_output = input_x.is_sign_negative();
+    let x_input = input_x.abs();
+
+    let x_n_primeof_x = (-0.5_f32 * x_input.powi(2)).exp() * INV_SQRT_2PI;
+
+    let x_k2 = 1.0 / (1.0 + (0.2316419 * x_input));
+    let x_k2_2 = x_k2.powi(2);
+    let x_k2_3 = x_k2 * x_k2_2;
+    let x_k2_4 = x_k2 * x_k2_3;
+    let x_k2_5 = x_k2 * x_k2_4;
+
+    let x_local1 = x_k2 * 0.319381530;
+    let mut x_local2 = x_k2_2 * (-0.356563782);
+    let mut acc = x_k2_3 * 1.781477937;
+    x_local2 += acc;
+    acc = x_k2_4 * (-1.821255978);
+    x_local2 += acc;
+    acc = x_k2_5 * 1.330274429;
+    x_local2 += acc;
+
+    let x_local = 1.0 - ((x_local2 + x_local1) * x_n_primeof_x);
+
+    if invert_output {
+        1.0 - x_local
+    } else {
+        x_local
+    }
+}
+
+/// Verifies the results calculated.
+///
+/// Both slices must have the same ordering of data and be equally long.
+pub fn verify_all_results(options: &[OptionData], results: &[f32]) -> usize {
+    assert_eq!(options.len(), results.len());
+
+    let mut err_count = 0;
+    for idx in 0..options.len() {
+        if !options[idx].verify(results[idx]) {
+            err_count += 1;
+        }
+    }
+
+    err_count
 }
